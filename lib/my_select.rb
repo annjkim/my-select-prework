29a2e5214d3dc4c5f1996018e10b075dd219bc56
Ann Kim <annkim822@gmail.com>
2018-06-21 16:08:44 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,14 @@
-def my_select(collection)
- i = 0
- new_collection = []
-  while i < collection.size
-    if yield()
-  
-  
-  
-  
+
+def my_select(array)
+  i = 0
+  new_array = []
+  while i < array.size
+    if yield(array[i]) == true
+      new_array << array[i]
+    else
+      #nothing
+    end
+    i += 1
+  end
+  new_array
 end
